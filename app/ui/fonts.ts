4e56a60f18f4ec66601fc7bf1959479554dfcd6e
lenakang <kangnara92@gmail.com>
2024-12-24 18:49:31 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lusitana, Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
